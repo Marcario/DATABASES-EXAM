5309bf28a94083b0a8ec3230dfa70f62263a2306
NALUBWAMA CHRISTINE <107610624+Marcario@users.noreply.github.com>
2023-01-19 15:12:49 +0300

    "DATABASES"

---
+++ QN6.sql
@@ -0,0 +1,48 @@
+-- qn6a creating tables
+-- CREATE TABLE employees(Id INT PRIMARY KEY NOT NULL,
+-- 					   NAME VARCHAR(100)NOT NULL,
+-- 					   AGE INT NOT NULL,
+-- 					   ADDRESS VARCHAR(255)NOT NULL
+-- 					  );
+					  
+					  
+-- 	inserting data into the tables				  
+-- INSERT INTO employees(Id,name,age,address)
+-- VALUES(1001,'Rohan',26,'Delhi'),
+-- 	   (1002,'Ankit',30,'Gurgaon'),
+-- 	   (1003,'Gaurav',27,'Mumbai'),
+-- 	   (1004,'Raja',32,'Nagpur');  
+
+
+-- qn6a selecting details for employees with id as 1004
+-- SELECT * FROM employees
+-- WHERE Id=1004;
+
+-- -- qn6b displaying the records for all employees
+SELECT *FROM employees;
+
+-- qn6c displaying records of employees whose name starts with 'R'
+SELECT * FROM employees
+WHERE name='R';
+
+
+-- qn6 d
+-- selecting employees according to ascending order of their age
+SELECT Id,age,name
+FROM employees
+ORDER BY age  ASC;
+
+--   selecting employees according to descending order of their age.
+
+SELECT Id,age,name
+FROM employees
+ORDER BY age  DESC;
+
+
+-- qn6e using the select distinct command
+-- SELECT DISTINCT address FROM employees;
+
+
+
+
+
